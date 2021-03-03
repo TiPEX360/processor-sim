6c8952358806fcdf92626fa04620dd03fdd3eb24
Dziurawy Beret <dziurawy-beret@pop-os.localdomain>
2021-03-03 17:58:53 +0000

    works

---
+++ assembler.cpp
@@ -0,0 +1,64 @@
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+enum opcode {MOV, ADD, MUL, SUB, LOAD, STORE, BRANCH_LT, BRANCH_NZ, BRANCH, HALT};
+
+
+std::vector<std::string> split(const std::string &line, char delimiter) {
+	std::string haystack = line;
+	std::vector<std::string> tokens;
+	size_t pos;
+	while ((pos = haystack.find(delimiter)) != std::string::npos) {
+		tokens.push_back(haystack.substr(0, pos));
+		haystack.erase(0, pos + 1);
+	}
+	// Push the remaining chars onto the vector
+	tokens.push_back(haystack);
+	return tokens;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 3) return 1;
+
+    std::string inPath = argv[1];
+    std::string outPath = argv[2];
+    std::ifstream in(inPath, std::ifstream::in);
+    std::ofstream out(outPath, std::ofstream::out | std::ofstream::binary);
+
+    std::string line;
+    // std::getline(in, line);
+    while(!in.eof()) {
+        std::getline(in, line);
+        char instr[4] = {0x00, 0x00, 0x00, 0x00};
+
+        std::vector<std::string> tokens = split(line, ' ');
+
+        if(tokens[0].compare("mov") == 0) instr[0] = MOV;
+        else if(tokens[0].compare("add") == 0) instr[0] = ADD;
+        else if(tokens[0].compare("mul") == 0) instr[0] = MUL;
+        else if(tokens[0].compare("sub") == 0) instr[0] = SUB;
+        else if(tokens[0].compare("ldm") == 0) instr[0] = LOAD;
+        else if(tokens[0].compare("stm") == 0) instr[0] = STORE;
+        else if(tokens[0].compare("blt") == 0) instr[0] = BRANCH_LT;
+        else if(tokens[0].compare("bnz") == 0) instr[0] = BRANCH_NZ;
+        else if(tokens[0].compare("b") == 0) instr[0] = BRANCH;
+        else if(tokens[0].compare("halt") == 0) instr[0] = HALT;
+        else if(tokens[0].compare("") == 0 ) continue;
+
+        if(tokens.size() == 4) {
+            instr[1] = atoi(tokens[1].substr(1, tokens[1].size() - 1).c_str());
+            instr[2] = atoi(tokens[2].substr(1, tokens[2].size() - 1).c_str());
+            instr[3] = atoi(tokens[3].substr(1, tokens[3].size() - 1).c_str());
+        }
+
+        out.write((char *)instr, sizeof(char)*4);
+    }
+
+    in.close();
+    out.close();
+
+    return 0;
+}
---
+++ proc.cpp
@@ -0,0 +1,133 @@
+// Partial pseudo code for the interim submission, written in C
+// Not shown - Fetch() and Decode() stages
+// Only a small selection of simple instructions shown
+#include <iostream>
+#include <fstream>
+
+
+enum opcode {MOV, ADD, MUL, SUB, LOAD, STORE, BRANCH_LT, BRANCH_NZ, BRANCH, HALT};
+
+struct state {
+
+};
+
+struct instr {
+    char opcode;
+    signed char r;
+    signed char a1;
+    signed char a2;
+    uint16_t target_addr; //use separate register to combine a1 and a2 range
+};
+
+int Execute(int opcode, int r, int a1, int a2, int *RF, int* MEM, int *PC, int target_addr, int *finished) {
+	int error = 0;
+	switch(opcode) {
+		case ADD:
+			RF[r] = RF[a1] + RF[a2];
+            (*PC)++;
+            break;
+		case MUL:
+			RF[r] = RF[a1] * RF[a2];
+            (*PC)++;
+            break;
+        case SUB:
+            RF[r] = RF[a1] - RF[a2];
+            (*PC)++;
+            break;
+		case LOAD:
+			RF[r] = MEM[ RF[a1] + RF[a2] ];
+            (*PC)++;
+            break;
+		case STORE:
+			MEM[ RF[a1] + RF[a2] ] = RF[r];
+            (*PC)++;
+            break;
+		case BRANCH_LT:
+			if (RF[a1] < RF[a2]) *PC = target_addr;
+            break;
+		case BRANCH_NZ:
+			if (a1 != 0) *PC = target_addr;
+            break;
+		case BRANCH:
+			*PC = target_addr;
+            break;
+		case HALT:
+			*finished = true;
+            break;
+		default:
+			std::cout << "Error: Opcode not recognised: " << opcode << std::endl;
+            error = 1;
+            break;
+	}
+	return error;
+}
+
+int Fetch(int *CIR, int PC, int *INSTR) {
+    int error = 0;
+    if(PC < 512) *CIR = INSTR[PC];
+    else {
+        std::cout << "Error: PC out of Memory range: " << PC << std::endl;
+        error = 1;
+    }
+    return error;
+}
+
+instr Decode(int CIR) {
+    instr i;
+    i.opcode = (CIR >> 24);
+    i.r = (CIR & 0x00FF0000) >> 16;
+    i.a1 = (CIR & 0x0000FF00) >> 8;
+    i.a2 = (CIR & 0x000000FF);
+    if((i.opcode >= BRANCH_LT) && (i.opcode <= BRANCH)) i.target_addr = i.a1 + i.a2;
+    return i;
+}
+
+void loadProgram(const char *path, int *INSTR) {
+    std::ifstream in(path, std::ifstream::in | std::ifstream::binary);
+    
+    int instrIndex = 0;
+    char instr[4];
+    in.read(instr, sizeof(char) * 4);
+    INSTR[instrIndex] = (int)((instr[0] << 24) | (instr[1] << 16) | (instr[2] << 8) | instr[3]);
+    instrIndex++;
+    while(!in.eof()) {
+        in.read(instr, 4);
+        INSTR[instrIndex] = (int)((instr[0] << 24) | (instr[1] << 16) | (instr[2] << 8) | instr[3]);
+        instrIndex++;
+    }
+}
+
+int main(int argc, char *argv[]) {
+	int finished = 0;
+	int cycles = 0;
+	int instructions = 0;
+
+	int RF[32];
+	int MEM[1024];
+    int *CIR = &RF[30];
+	int *PC = &RF[31];
+	int INSTR[512];
+
+    //RESET
+    *PC = 0;
+    instr decoded;
+
+    // Load program to memory
+    
+    if(argc > 1) loadProgram(argv[1], INSTR);
+    std::cout << INSTR[0] << std::endl;
+    std::cout << INSTR[1] << std::endl;
+    std::cout << INSTR[2] << std::endl;
+    std::cout << INSTR[3] << std::endl;
+    
+	while(!finished) {
+		Fetch(CIR, *PC, INSTR); //From memory to CIR in RF
+
+		decoded = Decode(*CIR); //Inspect CIR and determine opcode, r, a1, a2, 
+
+        Execute(decoded.opcode, decoded.r, decoded.a1, decoded.a2, RF, MEM, PC, decoded.target_addr, &finished);
+		
+        instructions++;
+		cycles += 3;
+	}
+}
